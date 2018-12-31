10aa5106cc0726a2271544b3353cc8227819d655
uboni <uday.boni@gmail.com>
2018-12-31 00:17:15 -0800

    main.py
    main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+Following is a simple program which demonstrate how python
+variable behaves
+Following are some of the meta-data extraced from the movie database for this
+example
+"""
+movie_title = "Avatar"
+director_name = "James Cameron"
+duration = 170.56
+title_year = "2009"
+
+print(movie_title)
+print(director_name)
+print(duration)
+print(title_year)
+print("\n")
+
+movie_title = "Pirates of the Caribbean: At World's End"
+director_name = "Gore Verbinski"
+duration = 169.23
+title_year = "2007"
+
+print(movie_title)
+print(director_name)
+print(duration)
+print(title_year)
+print("\n")
+
+
+
+movie_title = "Spectre"
+director_name = "Sam Mendes"
+duration = 148.00
+title_year = "2015"
+
+print(movie_title)
+print(director_name)
+print(duration)
+print(title_year)
+print("\n")
+
+
+movie_title = "The Dark Knight Rises"
+director_name = "Christopher Nolan"
+duration = 164.28
+title_year = "2012"
+
+print(movie_title)
+print(director_name)
+print(duration)
+print(title_year)
+print("\n")
