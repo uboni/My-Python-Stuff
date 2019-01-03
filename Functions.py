5236b270063c2593a095bf2d56e0c39107f436d7
uboni <uday.boni@gmail.com>
2019-01-02 22:50:41 -0800

    Homework Assignment #2: Functions
    Homework Assignment #2: Functions

---
+++ Functions.py
@@ -0,0 +1,29 @@
+
+
+"""
+3 different functions
+"""
+movie_title = "Avatar"
+director_name = "James Cameron"
+duration = 170.56
+title_year = "2009"
+
+
+def movie_title(input):
+    return input
+
+def director_name(input):
+    return input
+
+def title_year(input):
+    return input
+
+def isTrue(input):
+    if input == True:
+        return True
+    else:
+        return False
+print(movie_title("Avatar"))
+print(director_name("James Cameron"))
+print(title_year(2009))
+print(isTrue(True))
